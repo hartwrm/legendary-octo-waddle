a746c659b0890a0ac6f3a4c841ee307640a66fc2
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-05-26 22:29:55 -0400

    adding section 7

---
+++ section7/lesson17.py
@@ -0,0 +1,33 @@
+# dictionary data type - key value pairs
+
+
+myDog = {
+    'size': 'medium',
+    'color': 'black',
+    'behavior': 'shy'
+}
+
+print('my dog has ' + myDog['color'] + ' fur')
+
+print(list(myDog.keys()))
+print(list(myDog.values()))
+print(list(myDog.items()))
+
+for k in myDog.keys():
+    print(k)
+
+for v in myDog.values():
+    print(v)
+
+for k, v in myDog.items():
+    print(k, v)
+
+import pprint
+message = "it was  bright cold day in april, and the clocks were striking thirteen."
+count = {}
+
+for character in message.upper():
+    count.setdefault(character, 0)
+    count[character] = count[character] + 1
+
+pprint.pprint(count)

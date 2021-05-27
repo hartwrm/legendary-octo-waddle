986e922e9f46433cf6ff990cafb81887cf7d0b4e
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-05-26 20:55:25 -0400

    lesson 13 lists

--- section6/lesson13.py
+++ section6/lesson13.py
@@ -1,5 +1,30 @@
 #Test commits using new token
 
-print('test of a new git configs')
+# print('test of a new git configs')
 
 #and one last test
+
+spam  = 'hello'
+
+spam = [10,20,30]
+spam[1] = 'hello'
+
+spam[1:3] = 'cat', 'dog', 'mouse'
+
+print(spam)
+
+array = ['cat', 'bat', 'dog', 'tree']
+print(array[:2])
+print(array[1:])
+
+del array[3]
+print(array)
+print(len(array))
+
+print('hell0' * 3)
+print(array * 3)
+
+print(list('whats going on'))
+
+print('howdy' in ['hello', 'howdy', 'hey'])
+print('cat' in ['hello', 'howdy', 'hey'])

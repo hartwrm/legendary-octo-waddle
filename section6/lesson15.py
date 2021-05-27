d49a617a783ac9d8de80b8ff1fac656dff2b595e
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-05-26 21:26:46 -0400

    basic methods

---
+++ section6/lesson15.py
@@ -0,0 +1,24 @@
+words = ['hello', 'hi', 'sup', 'ni-hao']
+print(words.index('hello'))
+
+# list data types have several useful methods
+
+# add to the end of a list
+words.append('phone')
+print(words)
+
+# add anywhere in the list - index then item to add
+words.insert(2, 'mouse')
+print(words)
+
+# remove - not the same as del - del words[index] - remove will only remove the first instance of an item in a list
+words.remove('sup')
+print(words)
+
+# sort - order a list - 
+randomNums = [2,4,3,5,66,2,98,343,2,-54,-5,.5]
+randomNums.sort()
+print(randomNums)
+
+randomNums.sort(reverse=True)
+print(randomNums)

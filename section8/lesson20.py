a4d0cde18994f97095ea8f04675ebdfc022a337d
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-06-01 22:19:03 -0400

    string methods

---
+++ section8/lesson20.py
@@ -0,0 +1,26 @@
+import pyperclip
+
+spam = 'hello world'
+
+print(spam.upper())
+
+# answer = input()
+
+# print('hello'.isalpha())
+
+# print('hello123'.isalpha())
+
+# print('hello123445'.isalnum())
+
+# print('     '.isspace())
+
+# print('hello world'.isspace())
+
+# print("This Is Title Case".istitle())
+
+# print('hello world'.title())
+
+
+# print('Hello Wolrd'.startswith('H'))
+
+print(','.join(['cats', 'rats', 'bats']))

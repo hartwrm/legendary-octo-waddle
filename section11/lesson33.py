0a4c6d7f5f091080ff7b76760ad554081be0c727
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-07-19 23:00:45 -0400

    quick clean up

--- section11/lesson33.py
+++ section11/lesson33.py
@@ -3,17 +3,17 @@
 import os
 #unlink = delete
 #without absolute path deletes file from current workign directory
-os.unlink('filename.txt')
+# os.unlink('filename.txt')
 
-#remove directory - folder must be empty!
-os.rmdir('/directory')
+# #remove directory - folder must be empty!
+# os.rmdir('/directory')
 
-import shutil
-#delete folder + all it's content
-shutil.rmtree('/directory/filename.txt')
+# import shutil
+# #delete folder + all it's content
+# shutil.rmtree('/directory/filename.txt')
 
 #dry run - comment out the actual delete commands and print instead
 for filename in os.listdir():
-    if filename.endswith('.txt')
+    if filename.endswith('.txt'):
     #os.unlink(filename)
-    print(filename)
+        print(filename)
--- section11/lesson34.py
+++ section11/lesson34.py
@@ -3,7 +3,7 @@
 import os
 
 #os.walk('/folder_to_check')
-for folderName, subFolders, fileNames in os.walk('/Users/achiv/Documents/devStuff/pythonCourses/AutomateTheBoringStuff/legendary-octo-waddle/section11'):
+for folderName, subFolders, fileNames in os.walk('/absoulte/directory/path'):
     print('The folder is: ' + folderName)
     print('subfolders are: ' + str(subFolders))
     print('filenames are: ' + str(fileNames))

82152ada1a50f37d174df0928b19e1ae397ba9aa
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-07-19 22:36:59 -0400

    test commit

---
+++ section11/lesson33.py
@@ -0,0 +1,22 @@
+#Deleting files
+
+import os
+#unlink = delete
+#without absolute path deletes file from current workign directory
+os.unlink('filename.txt')
+
+#remove directory - folder must be empty!
+os.rmdir('/directory')
+
+import shutil
+#delete folder + all it's content
+shutil.rmtree('/directory/filename.txt')
+
+#dry run - comment out the actual delete commands and print instead
+for filename in os.listdir():
+    if filename.endswith('.txt')
+    #os.unlink(filename)
+    print(filename)
+
+
+    test test test

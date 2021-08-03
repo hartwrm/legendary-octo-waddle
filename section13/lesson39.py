c84265f3821876e25f14846950dfefac04d5c18c
Andrew Hartzell <achiv@Andrews-MBP.fios-router.home>
2021-08-02 21:27:07 -0400

    requests module

---
+++ section13/lesson39.py
@@ -0,0 +1,19 @@
+#Lesson 39 - Downloading from the web with Requests Module
+import requests
+
+res = requests.get('https://automatetheboringstuff.com/files/rj.txt')
+print(res.status_code)
+
+print(len(res.text))
+
+print(res.text[:100])
+
+# res.raise_for_status()
+# badRes = requests.get("https://automatetheboringstuff.com/asdfkljadsl;fkja")
+# print(badRes.raise_for_status())
+
+playFile = open('RomeoAndJuliet.txt', 'wb')
+for chunk in res.iter_content(100000):
+    playFile.write(chunk)
+
+playFile.close()

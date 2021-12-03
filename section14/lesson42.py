ff9b15deb5508e11da11c014c37d8c36c9a55d1a
Andrew Hartzell <achiv@Andrews-MacBook-Pro.local>
2021-12-02 21:20:54 -0500

    lesson 42 reading excel spreadsheets

---
+++ section14/lesson42.py
@@ -0,0 +1,12 @@
+import openpyxl
+import os
+os.chdir('/Users/achiv/Downloads')
+from openpyxl import load_workbook
+
+workbook = load_workbook('example_excel.xlsx')
+print(workbook.sheetnames)
+sheet = workbook['Sheet1']
+print(sheet['A1'].value)
+
+for i in range (1, 8):
+    print(i, sheet.cell(row=i, column=2).value)
